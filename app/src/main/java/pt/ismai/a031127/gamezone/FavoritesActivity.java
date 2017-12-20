0ff0180f2eb08791754471a4dc6f4938c23e5bab
BrunoPoupado <Skylo_mixes@hotmail.com>
2017-12-20 18:41:19 +0000

    Criado todo o Layout do Projeto.
    Adicionado Ficheiro do Gradle do Projeto

---
+++ app/src/main/java/pt/ismai/a031127/gamezone/FavoritesActivity.java
@@ -0,0 +1,13 @@
+package pt.ismai.a031127.gamezone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavoritesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
---
+++ app/src/main/java/pt/ismai/a031127/gamezone/WishListActivity.java
@@ -0,0 +1,13 @@
+package pt.ismai.a031127.gamezone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WishListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish_list);
+    }
+}
